ddb86d8574ec882e0c3d8415c233ca7db4af2189
wadaniel <wadaniel@ethz.ch>
2023-12-21 15:28:41 +0100

    simple visualizer

--- simplevisualizer.py
+++ simplevisualizer.py
@@ -13,6 +13,15 @@ from OCC.Core.StepRepr import StepRepr_RepresentationItem
 from OCC.Core.AIS import AIS_ColoredShape
 from OCC.Display.SimpleGui import init_display
 
+from OCC.Core.GeomAbs import GeomAbs_Plane, GeomAbs_Cylinder, GeomAbs_BSplineSurface
+from OCC.Core.BRepAdaptor import BRepAdaptor_Surface
+from OCC.Core.TopoDS import TopoDS_Face
+from OCC.Display.SimpleGui import init_display
+from OCC.Extend.DataExchange import read_step_file
+from OCC.Extend.TopologyUtils import TopologyExplorer
+
+shape = None
+
 def read_step(filename):
     """Reads STEP file with labels on each B-Rep face."""
     if not os.path.exists(filename):
@@ -27,6 +36,7 @@ def read_step(filename):
     return shape
 
 def display():
+    global shape
     global shape_index
     global shape_paths
 
@@ -78,6 +88,60 @@ def show_random():
     shape_index = random.randrange(0, len(shape_paths))
     display()
 
+
+def recognize_face(a_face):
+    """Takes a TopoDS shape and tries to identify its nature
+    whether it is a plane a cylinder a torus etc.
+    if a plane, returns the normal
+    if a cylinder, returns the radius
+    """
+    if not type(a_face) is TopoDS_Face:
+        print("Please hit the 'G' key to switch to face selection mode")
+        return False
+    surf = BRepAdaptor_Surface(a_face, True)
+    surf_type = surf.GetType()
+    if surf_type == GeomAbs_Plane:
+        print("Identified Plane Geometry")
+        # look for the properties of the plane
+        # first get the related gp_Pln
+        gp_pln = surf.Plane()
+        location = gp_pln.Location()  # a point of the plane
+        normal = gp_pln.Axis().Direction()  # the plane normal
+        # then export location and normal to the console output
+        print(
+            "--> Location (global coordinates)",
+            location.X(),
+            location.Y(),
+            location.Z(),
+        )
+        print("--> Normal (global coordinates)", normal.X(), normal.Y(), normal.Z())
+    elif surf_type == GeomAbs_Cylinder:
+        print("Identified Cylinder Geometry")
+        # look for the properties of the cylinder
+        # first get the related gp_Cyl
+        gp_cyl = surf.Cylinder()
+        location = gp_cyl.Location()  # a point of the axis
+        axis = gp_cyl.Axis().Direction()  # the cylinder axis
+        # then export location and normal to the console output
+        print(
+            "--> Location (global coordinates)",
+            location.X(),
+            location.Y(),
+            location.Z(),
+        )
+        print("--> Axis (global coordinates)", axis.X(), axis.Y(), axis.Z())
+    elif surf_type == GeomAbs_BSplineSurface:
+        print("Identified BSplineSurface Geometry")
+        # gp_bsrf = surf.Surface()
+        # degree = gp_bsrf.NbUKnots()
+        # TODO use a model that provided BSplineSurfaces, as1_pe_203.stp only contains
+        # planes and cylinders
+    else:
+        # TODO there are plenty other type that can be checked
+        # see documentation for the BRepAdaptor class
+        # https://www.opencascade.com/doc/occt-6.9.1/refman/html/class_b_rep_adaptor___surface.html
+        print(surf_type, "recognition not implemented")
+
 def recognize_clicked(shp, *kwargs):
     """ This is the function called every time
     a face is clicked in the 3d view
@@ -85,19 +149,31 @@ def recognize_clicked(shp, *kwargs):
 
     for shape in shp:
         print("Face selected: ", shape)
-        print("Face selected: ", shape.Location().HashCode(10000))
+        print("Hash: ", shape.__hash__())
+        recognize_face(shape)
+
+def recognize_batch(event=None):
+    """Menu item : process all the faces of a single shape"""
+    global shape
+
+    # loop over faces only
+    for face in TopologyExplorer(shape).faces():
+        # call the recognition function
+        recognize_face(face)
 
 if __name__ == '__main__':
 
     # User Defined
-    dataset_dir = "data"
+    #dataset_dir = "data"
     #dataset_dir = "more_data"
     #dataset_dir = "data_suter"
     #dataset_dir = "data_misc"
+    dataset_dir = "data_autogen"
 
     occ_display, start_occ_display, add_menu, add_function_to_menu = init_display("pyqt6")
     
-    occ_display.SetSelectionModeVertex() # This is the required function
+    #occ_display.SetSelectionModeVertex() # This is the required function
+    occ_display.SetSelectionModeFace()
     occ_display.register_select_callback(recognize_clicked)
 
     add_menu('explore')
@@ -106,6 +182,9 @@ if __name__ == '__main__':
     add_function_to_menu('explore', show_previous)
     add_function_to_menu('explore', show_first)
     add_function_to_menu('explore', show_last)
+
+    add_menu("recognition")
+    add_function_to_menu("recognition", recognize_batch)
 
     shape_paths = glob.glob(os.path.join(dataset_dir, '*.step'))
     if len(shape_paths) > 0:
